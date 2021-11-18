40bd929ee4a21b2439c8acb318bbd9dfcb54268d
gyeongseoMin <67200721+gyeongseoMin@users.noreply.github.com>
2021-11-18 16:54:19 +0900

    Update scrap/category page

---
+++ app/src/main/java/com/example/coo_eat/ScrapRecycleActivity.kt
@@ -0,0 +1,11 @@
+package com.example.coo_eat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScrapRecycleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.scrap_recycle)
+    }
+}
