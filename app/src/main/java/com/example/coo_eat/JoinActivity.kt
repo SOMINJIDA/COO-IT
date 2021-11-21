5d5bfab21e875abcc63fb04767ce93bc3407164c
dan-2ee <ekdms7273@naver.com>
2021-11-21 23:25:15 +0900

    - connect page - modify manifest file

--- app/src/main/java/com/example/coo_eat/JoinActivity.kt
+++ app/src/main/java/com/example/coo_eat/JoinActivity.kt
@@ -4,11 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_join.*
+import kotlinx.android.synthetic.main.activity_login.*
 
 
 class JoinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+        // 뒤로가기 버튼 클릭
+        btn_join_back.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 회원가입 버튼 클릭
+        btn_join_complete.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/coo_eat/LoginActivity.kt
+++ app/src/main/java/com/example/coo_eat/LoginActivity.kt
@@ -4,11 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // 회원가입 버튼 클릭
+        btn_signup.setOnClickListener{
+            val intent= Intent(this,JoinActivity::class.java)
+            startActivity(intent)
+        }
+        // 로그인 버튼 클릭
+        btn_login.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
