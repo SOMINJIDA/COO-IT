126ac67484c2e13cbe6e7bc2d26562f3a9884c62
dan-2ee <ekdms7273@naver.com>
2021-11-23 20:10:09 +0900

    add category layout & connect category button

--- app/src/main/java/com/example/coo_eat/Category2Activity.kt
+++ app/src/main/java/com/example/coo_eat/Category2Activity.kt
@@ -3,32 +3,31 @@ package com.example.coo_eat
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.category2.*
 import kotlinx.android.synthetic.main.category_1.*
-import kotlinx.android.synthetic.main.scrap.*
 
-class Category1Activity : AppCompatActivity() {
+class Category2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.category_1)
+        setContentView(R.layout.category2)
 
         // 뒤로가기 버튼 클릭
-        btn_category1_back.setOnClickListener {
+        btn_category2_back.setOnClickListener {
             val intent = Intent(this, RecipeActivity::class.java)
             startActivity(intent)
         }
         // 카테고리 이동
-        btn_category1_rice.setOnClickListener {
-            val intent = Intent(this, Category2Activity::class.java)
+        btn_category2_soup.setOnClickListener {
+            val intent = Intent(this, Category1Activity::class.java)
             startActivity(intent)
         }
-        btn_category1_vegan.setOnClickListener {
+        btn_category2_vegan.setOnClickListener {
             val intent = Intent(this, Category3Activity::class.java)
             startActivity(intent)
         }
-        btn_category1_snack.setOnClickListener {
+        btn_category2_snack.setOnClickListener {
             val intent = Intent(this, Category4Activity::class.java)
             startActivity(intent)
         }
     }
-
 }
