9f67b2972172977270900821cf206524d7551779
JisuuungKim <cham0810@kookmin.ac.kr>
2021-11-24 23:00:15 +0900

    Update firebase

--- app/src/main/java/com/example/coo_eat/JoinActivity.kt
+++ app/src/main/java/com/example/coo_eat/JoinActivity.kt
@@ -2,15 +2,33 @@ package com.example.coo_eat
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_join.*
-import kotlinx.android.synthetic.main.activity_login.*
 
 
 class JoinActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth //firebase 인스턴스 선언
+
+    val db = Firebase.firestore
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+
+        auth = Firebase.auth
+        val TAG:String = "MainActivity : " //log출력을 위한 TAG
+
+
         // 뒤로가기 버튼 클릭
         btn_join_back.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
@@ -21,6 +39,46 @@ class JoinActivity : AppCompatActivity() {
         btn_join_complete.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
+
+            //생성하고 싶은 이메일,비밀번호 id값 가져오기
+            val newEmail : EditText = findViewById(R.id.newEmail)
+            val newPassword : EditText = findViewById(R.id.newPassword)
+
+            //생성하고 싶은 이메일,비밀번호 text 가져오기
+            val newEmailText = newEmail.getText().toString()
+            val newPasswordText = newPassword.getText().toString()
+
+
+
+            val data = hashMapOf("" to true) //문서 빈 데이터 넣기
+
+
+            //신규사용자추가
+            auth.createUserWithEmailAndPassword(newEmailText, newPasswordText).addOnCompleteListener { task ->
+
+                if (task.isSuccessful) {
+//                        // 신규가입성공
+//
+                        db.collection(newEmailText).document("ingredient").set(data)
+                            .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
+                            .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+
+                        db.collection(newEmailText).document("scrap").set(data)
+                            .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
+                            .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+
+                        Toast.makeText(baseContext, "회원가입이 완료되었습니다",
+                            Toast.LENGTH_SHORT).show()
+//
+                    } else {
+                        // 신규가입실패
+                        Log.w(TAG,"createUserWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "정보를 다시 입력해주세요",
+                            Toast.LENGTH_SHORT).show()
+//
+                    }
+                }
+
         }
     }
 }
--- app/src/main/java/com/example/coo_eat/LoginActivity.kt
+++ app/src/main/java/com/example/coo_eat/LoginActivity.kt
@@ -2,15 +2,45 @@ package com.example.coo_eat
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
+import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth //firebase 인스턴스 선언
+
+    val db = Firebase.firestore
+
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
+        val TAG:String = "MainActivity : " //log출력을 위한 TAG
+
+        //이메일,비밀번호 id값으로 가져오기
+        val email : EditText = findViewById(R.id.email)
+        val password : EditText = findViewById(R.id.password)
+
 
         // 회원가입 버튼 클릭
         btn_signup.setOnClickListener{
@@ -19,8 +49,37 @@ class LoginActivity : AppCompatActivity() {
         }
         // 로그인 버튼 클릭
         btn_login.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+
+            //이메일,비밀번호 text 가져오기
+            val emailText = email.getText().toString()
+            val passwordText = password.getText().toString()
+
+
+            //사용자 확인후 로그인
+            auth.signInWithEmailAndPassword(emailText, passwordText).addOnCompleteListener(this) { task ->
+
+                if (task.isSuccessful) {
+
+                    //todo: 밑에 코드 세개 재료화면에 적용하기
+                    db.collection(emailText).document("ingredient").update("양파",true)
+                        .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
+                        .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+
+                        //화면전환
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+
+//
+                    } else {
+                        // 로그인 실패
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+
+                        //알림메시지
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+//
+                    }
+                }
         }
     }
 }
