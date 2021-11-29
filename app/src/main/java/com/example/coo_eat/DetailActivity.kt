541507c0a402e6a66d9c57fc62ca4ac02b480600
JisuuungKim <cham0810@kookmin.ac.kr>
2021-11-29 18:57:57 +0900

    상세 페이지 다했읍니다 ~

--- app/src/main/java/com/example/coo_eat/DetailActivity.kt
+++ app/src/main/java/com/example/coo_eat/DetailActivity.kt
@@ -3,14 +3,19 @@ package com.example.coo_eat
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.Color
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.annotation.Dimension
+import com.bumptech.glide.Glide
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -61,7 +66,65 @@ class DetailActivity : AppCompatActivity() {
 
         //레시피 이름 가져오기
         val thisRecipe = intent.getStringExtra("recipeName")
+        val thisImage = intent.getStringExtra("recipeImage")
+        val thisCategory = intent.getStringExtra("recipeCategory")
+        val thisIngredient = intent.getStringExtra("recipeIngredient")
+        val thisCookings = intent.getStringArrayListExtra("cookings")
+
+
         println(thisRecipe)
+        println(thisCategory)
+        println(thisIngredient)
+
+        //상세 데이터 보여지게
+
+        val recipeName = findViewById(R.id.recipe_name) as TextView
+        recipeName.setText(thisRecipe) //이름 수정
+
+        val recipeImage = findViewById(R.id.detailMain) as ImageView
+        Glide.with(this@DetailActivity).load(thisImage).into(recipeImage) //메인 사진 수정
+
+        val recipeCategory = findViewById(R.id.detailCategory) as TextView
+        recipeCategory.setText(thisCategory) //카테고리 수정
+
+        val recipeIngredient = findViewById(R.id.detailIngredient) as TextView
+        recipeIngredient.setText(thisIngredient) //재료 수정
+
+        val cookingLayout : LinearLayout = findViewById(R.id.detailCooking)
+
+        val layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+        )
+
+        layoutParams.gravity = Gravity.CENTER
+        layoutParams.setMargins(0,15,0,0)
+
+        if (thisCookings != null) {
+            for (i in 0..(thisCookings.toArray().size -1)) {
+                println(thisCookings[i])
+
+                var cooking = TextView(this)
+
+                cooking.layoutParams = layoutParams
+
+                cooking.setText(thisCookings[i])
+                cooking.setTextSize(Dimension.SP,15.0f)
+                cooking.setTextColor(Color.BLACK)
+
+                cookingLayout.addView(cooking)
+
+
+
+
+            }
+        }
+
+
+
+
+
+
 
 
 
